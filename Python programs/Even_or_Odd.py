39ae67b8c9bb256a7452873ac0cbd023c4149569
Jamma Hemanth Kumar <118712671+JammaHemanth@users.noreply.github.com>
2024-02-09 13:40:02 +0530

    Delete Python programs/Even_or_Odd.py

--- Python programs/Even_or_Odd.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter a number: "))
-while n > 0:
-                    if n%2 == 0:
-                            print(n, 'is a even number')
-                    else:
-                            print(n, 'is a odd number')
-                            
