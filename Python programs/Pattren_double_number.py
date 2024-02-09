2526b2d728e07be4b0987f19e4a0a2f43552c32a
Jamma Hemanth Kumar <118712671+JammaHemanth@users.noreply.github.com>
2024-02-09 13:40:29 +0530

    Delete Python programs/Pattren_double_number.py

--- Python programs/Pattren_double_number.py
+++
@@ -1,7 +0,0 @@
-rows = int(input("Enter a number: "))
-for i in range(1,rows+1):
-                    for j in range(1,i+1):
-                            print(i, end="\n")
-                          
- 
-
