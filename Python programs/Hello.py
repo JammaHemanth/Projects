3762f2665cf7147721c8d8052fba42c8cd444c43
Jamma Hemanth Kumar <118712671+JammaHemanth@users.noreply.github.com>
2024-02-09 12:39:37 +0530

    Update Hello.py

--- Python programs/Hello.py
+++ Python programs/Hello.py
@@ -1,11 +1,3 @@
-'''
-for i in range(10,50,10):
-                    print(i) 
-'''
-
-rows = int(input("Enter number of rows: "))
-
-for i in range(rows):
-    for j in range(i+1):
-        print(j+1, end=" ")
-    print()
+print(
+  "Hello World"
+)
