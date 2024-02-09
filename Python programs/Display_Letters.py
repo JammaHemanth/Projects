4af4182fdd38195df3c6e3ebe82f95eb2f1fc433
Jamma Hemanth Kumar <118712671+JammaHemanth@users.noreply.github.com>
2024-02-09 12:37:29 +0530

    Update Display_Letters.py

--- Python programs/Display_Letters.py
+++ Python programs/Display_Letters.py
@@ -4,11 +4,3 @@ x = " "
 for i in word:
                     x+=i
                     print(x)
-                    
-#Print Pattren
-rows = 5
-for i in range(1, rows+1):
-                    for j in range(1,i+1):
-                            print(j ,end=" ")
-                    print()        
-                            
