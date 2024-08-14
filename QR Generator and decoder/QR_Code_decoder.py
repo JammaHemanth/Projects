ad1043b3cdf22ef511cd5f944e90ff6c313e7758
Jamma Hemanth Kumar <118712671+JammaHemanth@users.noreply.github.com>
2024-08-14 16:19:39 +0530

    Delete QR Generator and decoder directory

--- QR Generator and decoder/QR_Code_decoder.py
+++
@@ -1,4 +0,0 @@
-from pyzbar.pyzbar import decode
-from PIL import Image
-decodeQR = decode(Image.open('instagram.png'))
-print(decodeQR[0].data.decode('ascii'))
--- QR Generator and decoder/QR_code_generator.py
+++
@@ -1,5 +0,0 @@
-import png
-import pyqrcode
-link = "https://thecleverprogrammer.com/2021/01/14/python-projects-with-source-code/"
-qr_code = pyqrcode.create(link)
-qr_code.png("instagram.png",scale=5)
