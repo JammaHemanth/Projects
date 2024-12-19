bbbd976df1012c9b2f5f935c6e6c0c0a1d966050
Jamma Hemanth <jammahemanthkumar@gmail.com>
2024-12-19 17:32:54 +0530

    Initial commit

--- Python programs/pallindrone_using_if_else.py
+++
@@ -1,8 +0,0 @@
-s = input("Enter the value: ")
-
-reverse = s[::-1]
-
-if s == reverse:
-                    print("yes, it's a plaindrone number")
-else:
-        print("no, it's not a pallindrome number")                    
